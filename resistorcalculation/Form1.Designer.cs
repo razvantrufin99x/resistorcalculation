036b97e97c8baa8e3bd5945565bb74ce09472db8
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-04-03 23:36:39 +0300

    Add project files.

---
+++ resistorcalculation/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace resistorcalculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtU = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtI = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(118, 38);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(165, 22);
+            this.txtR.TabIndex = 0;
+            this.txtR.Text = "50.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "U";
+            // 
+            // txtU
+            // 
+            this.txtU.Location = new System.Drawing.Point(118, 84);
+            this.txtU.Name = "txtU";
+            this.txtU.Size = new System.Drawing.Size(165, 22);
+            this.txtU.TabIndex = 2;
+            this.txtU.Text = "5.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "I";
+            // 
+            // txtI
+            // 
+            this.txtI.Location = new System.Drawing.Point(118, 131);
+            this.txtI.Name = "txtI";
+            this.txtI.Size = new System.Drawing.Size(165, 22);
+            this.txtI.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "P";
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(118, 178);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(165, 22);
+            this.txtP.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(449, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "calculeaza Rezistenta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(449, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "calculeaza Tensiunea";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(449, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "calculeaza Curentul";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(449, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "calculeaza Puterea";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(317, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Watt wati [W]";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(317, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Amperi [A]";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(317, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Volti [V]";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(317, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Ohmi [Omega]";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(712, 178);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 16);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "P = I * U ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(712, 131);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 16);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "I = U / R";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(712, 84);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 16);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "U = R * I";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(712, 38);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(53, 16);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "R = U / I";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 243);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtI);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtU);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtR);
+            this.Name = "Form1";
+            this.Text = "calcule rezistor in curent continuu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtU;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtI;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ resistorcalculation/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace resistorcalculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public float R = 0.0f;
+        public float U = 0.0f;
+        public float I = 0.0f;
+        public float P = 0.0f;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //stiind tensiunea U si curentul I calculeaza Rezistenta R in Ohmi [omega][gre] dupa formula
+            //R = U/I
+            try
+            {
+                U = (float)Convert.ToDouble(txtU.Text);
+            }
+            catch { }
+            try
+            {
+                I = (float)Convert.ToDouble(txtI.Text);
+            }
+            catch { }
+            try
+            {
+                R = U / I;
+            }
+            catch { }
+            txtR.Text = R.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //stiind rezistenta R si curentul I calculeaza Tensiunea U in Volti [V] dupa formula
+            //U = R * I
+            try
+            {
+                R = (float)Convert.ToDouble(txtR.Text);
+            }
+            catch { }
+            try
+            {
+                I = (float)Convert.ToDouble(txtI.Text);
+            }
+            catch { }
+            try
+            {
+                U = R * I;
+            }
+            catch { }
+            txtU.Text = U.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //stiind rezistenta R si tensiune U calculeaza curentul I in Amperi [A] dupa formula
+            //I = U / R
+            try
+            {
+                R = (float)Convert.ToDouble(txtR.Text);
+            }
+            catch { }
+            try
+            {
+                U = (float)Convert.ToDouble(txtU.Text);
+            }
+            catch { }
+            try
+            {
+                I = U / R;
+            }
+            catch { }
+            txtI.Text = I.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //stiind curentul I si tensiune U calculeaza puterea P in wati [watt][W] dupa formula
+            //P = I * U 
+            try
+            {
+                I = (float)Convert.ToDouble(txtI.Text);
+            }
+            catch { }
+            try
+            {
+                U = (float)Convert.ToDouble(txtU.Text);
+            }
+            catch { }
+            try
+            {
+                P = U * I;
+            }
+            catch { }
+            txtP.Text = P.ToString();
+        }
+    }
+}
